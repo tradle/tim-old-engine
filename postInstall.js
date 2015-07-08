793649788da3077e89c62c5781e91813d37403ef
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-07-07 21:54:23 -0400

    more careful postinstall

--- postInstall.js
+++ postInstall.js
@@ -7,19 +7,26 @@ var nmDir = path.join(__dirname, 'node_modules/')
 
 var zlorpOtrDir = nmDir + 'zlorp/node_modules/otr'
 fs.exists(zlorpOtrDir, function (exists) {
-  if (exists) fs.move(zlorpOtrDir, nmDir + 'otr', rethrow)
+  if (exists) {
+    fs.move(zlorpOtrDir, nmDir + 'otr', function (err) {
+      if (err) throw err
+
+      clean()
+    })
+  } else clean()
 })
 
-;[
-  nmDir + 'midentity/node_modules/otr',
-  nmDir + 'tradle-verifier/node_modules/midentity',
-  nmDir + 'tradle-verifier/node_modules/chained-obj',
-  nmDir + 'bitkeeper-client-js/node_modules/chained-obj'
-].forEach(function (dir) {
-  fs.exists(dir, function (exists) {
-    if (exists) fs.remove(dir, rethrow)
+function clean () {
+  ;[
+    nmDir + 'midentity/node_modules/otr',
+    nmDir + 'tradle-verifier/node_modules/midentity',
+    nmDir + 'tradle-verifier/node_modules/chained-obj'
+  ].forEach(function (dir) {
+    fs.exists(dir, function (exists) {
+      if (exists) fs.remove(dir, rethrow)
+    })
   })
-})
+}
 
 function rethrow (err) {
   if (err) throw err
