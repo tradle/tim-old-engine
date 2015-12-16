6ca0b1e7ce9d25b6fdf58c607463f4f1d87749cb
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-12-15 20:23:25 -0500

    ignore identities that publish keys of existing identities

--- test/identityDB.js
+++ test/identityDB.js
@@ -1,7 +1,8 @@
 
 var test = require('tape')
-var extend = require('xtend/mutable')
+var extend = require('xtend')
 var leveldown = require('memdown')
+var Q = require('q')
 var collect = require('stream-collector')
 var constants = require('@tradle/constants')
 var Log = require('logbase').Log
@@ -10,42 +11,120 @@ var fakeKeeper = require('@tradle/test-helpers').fakeKeeper
 var createIdentityDB = require('../lib/identityDB')
 var ted = require('./fixtures/ted-pub')
 var EventType = require('../lib/eventType')
+var utils = require('../lib/utils')
+var TYPE = constants.TYPE
 var ROOT_HASH = constants.ROOT_HASH
+var PREV_HASH = constants.PREV_HASH
 var CUR_HASH = constants.CUR_HASH
+var IDENTITY_TYPE = constants.TYPES.IDENTITY
+var dbCounter = 0
+var nextDBName = function () {
+  return 'db' + (dbCounter++)
+}
 
-test('identity store', function (t) {
-  t.plan(7)
+test('ignore identities that collide on keys', function (t) {
+  t.plan(ted.pubkeys.length)
 
-  var log = new Log('log.db', {
+  var log = new Log(nextDBName(), {
     db: leveldown
   })
 
-  ted = extend({}, ted)
+  ted = extend(ted) // defensive copy
+  var badPerson = extend(ted, { name: 'evil ted' })
   var keeperMap = {}
-  var hash = ted[ROOT_HASH] = ted[CUR_HASH] = 'abc'
-  keeperMap[hash] = ted
-  var entry = new Entry(extend({ type: EventType.chain.readSuccess }, ted))
-  log.append(entry)
-  entry.set('name', 'bill')
-  log.append(entry)
+  var tedHash = 'abc'
+  var badPersonHash = 'efg'
+  keeperMap[tedHash] = ted
+  keeperMap[badPersonHash] = badPerson
+
+  var tedFromChain = new Entry({
+      type: EventType.chain.readSuccess
+    })
+    .set(TYPE, IDENTITY_TYPE)
+    .set(CUR_HASH, tedHash)
+    .set(ROOT_HASH, tedHash)
+
+  var badPersonFromChain = new Entry({
+      type: EventType.chain.readSuccess
+    })
+    .set(TYPE, IDENTITY_TYPE)
+    .set(CUR_HASH, badPersonHash)
+    .set(ROOT_HASH, badPersonHash)
+
+  log.append(tedFromChain)
+  log.append(badPersonFromChain)
 
   var keeper = fakeKeeper.forMap(keeperMap)
-  var identities = createIdentityDB('identities.db', {
+  var identities = createIdentityDB(nextDBName(), {
+    leveldown: leveldown,
+    log: log,
+    keeper: keeper
+  })
+
+  identities.onLive(function () {
+    Q.all(ted.pubkeys.map(function (k) {
+        return Q.ninvoke(identities, 'byFingerprint', k.fingerprint)
+      }))
+      .done(function (identities) {
+        identities.forEach(function (i) {
+          t.deepEqual(i.identity.name, ted.name)
+        })
+      })
+  })
+})
+
+test('update identity', function (t) {
+  t.plan(7)
+
+  var log = new Log(nextDBName(), {
+    db: leveldown
+  })
+
+  ted = extend(ted)
+
+  var keeperMap = {}
+  var originalHash = 'abc'
+  var entry = new Entry({
+      type: EventType.chain.readSuccess
+    })
+    .set(TYPE, IDENTITY_TYPE)
+    .set(CUR_HASH, originalHash)
+    .set(ROOT_HASH, originalHash)
+
+  keeperMap[originalHash] = ted
+  log.append(entry)
+
+  ted = extend(ted)
+  ted.name = 'ted!'
+
+  var updateHash = 'abc1'
+  var update = new Entry(entry.toJSON())
+    .set(CUR_HASH, updateHash)
+    .set(PREV_HASH, originalHash)
+
+  keeperMap[updateHash] = ted
+  log.append(update)
+
+  var keeper = fakeKeeper.forMap(keeperMap)
+  var identities = createIdentityDB(nextDBName(), {
     leveldown: leveldown,
     log: log,
     keeper: keeper
   })
 
   identities.on('change', function (id) {
-    if (id === 2) {
-      identities.byFingerprint(ted.pubkeys[0].fingerprint, function (err, storedTed) {
-        if (err) throw err
+    if (id !== 2) return
 
-        t.deepEqual(storedTed.identity, ted)
-      })
+    identities.byFingerprint(ted.pubkeys[0].fingerprint, function (err, storedTed) {
+      if (err) {
+        t.error(err)
+        throw err
+      }
 
-      testStreams()
-    }
+      t.deepEqual(storedTed.identity, ted)
+    })
+
+    testStreams()
   })
 
   function testStreams () {
@@ -60,7 +139,7 @@ test('identity store', function (t) {
       if (err) throw err
 
       t.equal(stored.length, 1)
-      t.equal(stored[0], ted[ROOT_HASH])
+      t.equal(stored[0], originalHash)
     })
 
     collect(identities.createValueStream(), function (err, stored) {
